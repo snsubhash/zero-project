79834badb94c1f5fabea88e7debfcf33e48c4def
Subhash <subhash@subhash.local>
2016-04-22 11:20:13 +0530

    Initial Commit

---
+++ ProjectTyphoonDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjectTyphoonDemo
+//
+//  Created by Subhash on 4/22/16.
+//  Copyright © 2016 Happiestminds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProjectTyphoonDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectTyphoonDemo
+//
+//  Created by Subhash on 4/22/16.
+//  Copyright © 2016 Happiestminds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
